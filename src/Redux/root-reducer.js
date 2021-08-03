1fb6056e8ce60935af52c274dfa55b6e33d34043
contractorakshar <contractorakshar@gmail.com>
2021-08-03 21:33:24 +0530

    ✅ ch 9 done

--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,8 +1,18 @@
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './User/user.reducer';
 import cartReducer from './Cart/cart.reducer';
 import { combineReducers } from 'redux';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './Redux/store';
+import { store, persistor } from './Redux/store';
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
